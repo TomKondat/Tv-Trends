f30bf58e6023685215f289ae091898a87c0cc243
Tom Kondat <tomkondat5@gmail.com>
2024-01-22 00:20:40 +0200

    Add favicon and fix header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
-      <Container>
+      <Container className="d-flex justify-content-center align-items-center">
         <Navbar.Brand as={Link} to="/">
           <img
             alt=""
@@ -14,7 +14,15 @@ function Header() {
             height="30"
             className="d-inline-block align-top"
           />{" "}
-          Movies-App
+          &nbsp; <span style={{ color: "#CE3B3B" }}>M</span>ovies-
+          <span style={{ color: "#CE3B3B" }}>A</span>pp &nbsp;
+          <img
+            alt=""
+            src="https://www.freeiconspng.com/thumbs/popcorn-png/popcorn-png-16.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
         </Navbar.Brand>
       </Container>
     </Navbar>
