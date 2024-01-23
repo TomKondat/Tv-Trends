b0ffb0503399740c63b8c523218a001866876511
Tom Kondat <tomkondat5@gmail.com>
2024-01-23 20:58:02 +0200

    fetch movies api and map them with cards

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
 function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=6f8ef2f5b313c678f8869b69affa2d31"
+    )
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
   return (
     <div>
-      <h2>Movie Page</h2>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 }
