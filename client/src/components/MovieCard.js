f66415b2f2293e698c2ee5972a992497f1bedabc
Tom Kondat <tomkondat5@gmail.com>
2024-01-26 22:00:45 +0200

    Adding overview to cards

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 import "./styles.css";
+import { CgDetailsMore } from "react-icons/cg";
 
 function MovieCard({ movie }) {
   const title = movie.title || movie.name;
+  const [showOverview, setShowOverview] = useState(false);
 
   const handleTrailer = () => {
     const searchTerm = `${title} trailer`;
@@ -13,6 +17,19 @@ function MovieCard({ movie }) {
     )}`;
     window.open(youtubeSearchUrl, "_blank");
   };
+
+  const handleOverview = () => {
+    setShowOverview(!showOverview);
+  };
+
+  const popover = (
+    <Popover id="popover-basic" className="overview-popover">
+      <Popover.Header as="h3">{title} Overview</Popover.Header>
+
+      <Popover.Body>{movie.overview}</Popover.Body>
+    </Popover>
+  );
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img
@@ -26,6 +43,7 @@ function MovieCard({ movie }) {
           Rating: {(movie.vote_average / 2).toFixed(1)} ({movie.vote_count}{" "}
           Votes)
         </Card.Text>
+
         <Button
           className="trailer-button"
           variant="danger"
@@ -33,6 +51,21 @@ function MovieCard({ movie }) {
         >
           Watch Trailer
         </Button>
+        <OverlayTrigger
+          trigger="click"
+          placement="top-end"
+          show={showOverview}
+          overlay={popover}
+        >
+          <Button
+            className="overview-button"
+            variant="secondary"
+            style={{ marginLeft: "70px" }}
+            onClick={handleOverview}
+          >
+            Overview
+          </Button>
+        </OverlayTrigger>
       </Card.Body>
     </Card>
   );
