8311bd891cf01b73f5f2b61311134948e8916a93
Tom Kondat <tomkondat5@gmail.com>
2024-02-02 16:52:18 +0200

    Add clear liked movies

--- client/src/components/LikedPage.js
+++ client/src/components/LikedPage.js
@@ -1,21 +1,41 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/esm/Button";
 
-const LikedPage = ({ likedMovies, handleLike }) => {
+function LikedPage({ likedMovies, handleLike, setLikedMovies }) {
+  const handleClearLiked = () => {
+    setLikedMovies([]);
+  };
   return (
-    <div>
-      <h1>Liked Movies</h1>
-      <div>
-        {likedMovies && Array.isArray(likedMovies) && likedMovies.length > 0 ? (
-          likedMovies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} handleLike={handleLike} />
-          ))
-        ) : (
-          <p>No liked movies yet</p>
-        )}
-      </div>
+    <div className="liked-container">
+      <Container>
+        <h1 style={{ color: "#CE3B3B" }} className="py-5">
+          Everything you've liked
+        </h1>
+        <Button
+          variant="danger"
+          onClick={handleClearLiked}
+          disabled={likedMovies.length === 0}
+        >
+          Clear all liked movies
+        </Button>
+        <div>
+          {likedMovies &&
+          Array.isArray(likedMovies) &&
+          likedMovies.length > 0 ? (
+            likedMovies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} handleLike={handleLike} />
+            ))
+          ) : (
+            <h1 style={{ color: "#CE3B3B" }} className="py-5">
+              No liked movies yet
+            </h1>
+          )}
+        </div>
+      </Container>
     </div>
   );
-};
+}
 
 export default LikedPage;
