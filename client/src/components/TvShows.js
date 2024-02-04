7d48086533a593a9313f9f47880c81db4435dcb2
Tom Kondat <tomkondat5@gmail.com>
2024-02-04 14:18:11 +0200

    Fix tv shows page to be like movies page

--- client/src/components/TvShows.js
+++ client/src/components/TvShows.js
@@ -5,8 +5,9 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Pagination from "react-bootstrap/Pagination";
 import Spinner from "react-bootstrap/Spinner";
+import { Button } from "react-bootstrap";
 
-function TvShows() {
+function Movies({ handleLike }) {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const moviesPerPage = 4;
@@ -30,19 +31,75 @@ function TvShows() {
   useEffect(() => {
     getMovies();
   }, []);
-
+  // pagination
   const indexOfLastMovie = currentPage * moviesPerPage;
   const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
   const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const handleRating = () => {
+    // Sort movies by vote_average in descending order
+    const sortedMovies = [...movies].sort(
+      (a, b) => b.vote_average - a.vote_average
+    );
+    setMovies(sortedMovies);
+  };
+
+  const handleNewest = () => {
+    // Sort movies by release_date in descending order
+    const sortedMovies = [...movies].sort(
+      (a, b) => new Date(b.release_date) - new Date(a.release_date)
+    );
+    setMovies(sortedMovies);
+  };
+
+  const handleVotes = () => {
+    // Sort movies by vote_count in descending order
+    const sortedMovies = [...movies].sort(
+      (a, b) => b.vote_count - a.vote_count
+    );
+    setMovies(sortedMovies);
+  };
+
+  const handleClear = () => {
+    getMovies();
+  };
+
+  console.log(movies);
   return (
     <div className="tvshows-container">
       <Container>
-        <div className="explore-heading">
+        <div>
           <h1 style={{ color: "#CE3B3B" }} className="py-5">
             Most Trending Tv Shows Right Now
           </h1>
+        </div>
+        <div className="filter-container mb-4">
+          <Button
+            className=" me-5"
+            variant="secondary"
+            size="sm"
+            onClick={handleClear}
+          >
+            Clear Filters
+          </Button>
+          <Button
+            variant="outline-warning"
+            className=" me-2"
+            onClick={handleRating}
+          >
+            Higher Rating
+          </Button>
+          <Button
+            className=" me-2"
+            variant="outline-warning"
+            onClick={handleNewest}
+          >
+            Latest Release
+          </Button>
+          <Button variant="outline-warning" onClick={handleVotes}>
+            Most Votes
+          </Button>
         </div>
         {loading ? (
           <Spinner
@@ -62,7 +119,7 @@ function TvShows() {
           <Row xs={1} md={2} lg={3} xl={3} xxl={4} className="g-4">
             {currentMovies.map((movie) => (
               <Col key={movie.id}>
-                <MovieCard movie={movie} />
+                <MovieCard movie={movie} handleLike={handleLike} />
               </Col>
             ))}
           </Row>
@@ -88,4 +145,4 @@ function TvShows() {
   );
 }
 
-export default TvShows;
+export default Movies;
