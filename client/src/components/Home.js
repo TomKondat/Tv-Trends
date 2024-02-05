2aa37f1291766e5104f44949b883fe688b6414f0
Tom Kondat <tomkondat5@gmail.com>
2024-02-05 23:37:49 +0200

    Change Header and style home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,43 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function Home() {
+  const phrases = [
+    "Explore everything that's Trending",
+    "Explore Movies",
+    "Explore TV Shows",
+  ];
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="home-container">
-      <div className="welcome-heading">
-        <span>
-          Welcome to <span style={{ color: "#CE3B3B" }}>T</span>v{" "}
-          <span style={{ color: "#CE3B3B" }}>T</span>rends
-        </span>
-      </div>
-      <div className="welcome-content">
-        <span>
-          Explore everything that's{" "}
-          <span style={{ color: "#CE3B3B", fontWeight: "bold" }}>Trending</span>
-        </span>
-      </div>
-      <Link to="/movies">
-        <Button
-          className="explore-movies"
-          size="lg"
-          variant="outline-danger"
-          active
-        >
-          Explore Movies
-        </Button>
-      </Link>
-      <Link to="/tvshows">
-        <Button
-          className="explore-tvshows"
-          size="lg"
-          variant="outline-primary"
-          active
-        >
-          Explore TV Shows
-        </Button>
-      </Link>
+      <Container>
+        <div className="welcome-heading pt-5 pb-2">
+          <span>Welcome to TV Trends</span>
+        </div>
+        <div className="welcome-content">
+          <span className="phrase-transition">
+            {phrases[currentPhraseIndex]}
+          </span>
+        </div>
+        <div className="mt-5 pt-5">
+          <div>
+            <Link to="/movies">
+              <Button
+                className="explore-movies"
+                size="lg"
+                variant="danger"
+                active
+              >
+                Explore Movies
+              </Button>
+            </Link>
+          </div>
+          <div className="mt-5">
+            <Link to="/tvshows">
+              <Button
+                className="explore-tvshows"
+                size="lg"
+                variant="primary"
+                active
+              >
+                Explore TvShows
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
